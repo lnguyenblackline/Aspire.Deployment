136ca1f8624716d56cb9901de482c4c49a232c6c
Luan Nguyen <lnguyen@blacklineit.com>
2024-12-02 12:15:13 -0600

    working on the deployment

--- Aspire.Deployment.ApiService/EndPoints/AllEndpoints.cs
+++ Aspire.Deployment.ApiService/EndPoints/AllEndpoints.cs
@@ -1,4 +1,5 @@
 ﻿using System.Text.Json;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Caching.Distributed;
 
 namespace Aspire.Deployment.ApiService.EndPoints
@@ -11,25 +12,23 @@ namespace Aspire.Deployment.ApiService.EndPoints
         };
         public static void SetUpAllEndpoints(this IEndpointRouteBuilder app)
         {
-            app.MapGet("/weatherforecast", async (IDistributedCache cache) =>
+            app.MapGet("/weatherforecast", () =>
             {
-                var cacheName = "redisCache";
-                var cacheData = await cache.GetAsync(cacheName);
-                if (cacheData is not null)
-                {
-                    using var ms = new MemoryStream(cacheData);
-                    return JsonSerializer.Deserialize<WeatherForecast[]>(ms);
-                }
                 var forecast = Enumerable.Range(1, 5).Select(index =>
-                        new WeatherForecast
-                        (
-                            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                            Random.Shared.Next(-20, 55),
-                            summaries[Random.Shared.Next(summaries.Length)]
-                        ))
+                    new WeatherForecast
+                    (
+                        DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                        Random.Shared.Next(-20, 55),
+                        summaries[Random.Shared.Next(summaries.Length)]
+                    ))
                     .ToArray();
                 return forecast;
             });
         }
+    }
+
+    record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+    {
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
     }
 }
--- Aspire.Deployment.ApiService/Program.cs
+++ Aspire.Deployment.ApiService/Program.cs
@@ -20,7 +20,3 @@ app.MapDefaultEndpoints();
 
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
